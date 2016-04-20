dd56983b3e6e0ecde627aef33e6065ae24eecf83
Eric Crescioni <ecam900@gmail.com>
2016-04-20 17:57:31 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThisDudeCodes::Application.config.secret_key_base = '2de2d01da205313a653d300f5131971e91cb84b69930338eb2ffcbc99107940fd69b12e9641a11801daf206a2ca0b6078b843467aa236a712eef284a8475709c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ThisDudeCodes::Application.config.session_store :cookie_store, key: '_ThisDudeCodes_session'
